2b53ec9d4214bfa28a06571faa5a2172d64bcb02
Malvin Denga <134788554+malvind007@users.noreply.github.com>
2023-12-05 20:57:53 +0200

    login sign up page

--- src/Components/Breadcrums/Breadcrum.jsx
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -3,11 +3,12 @@ import './breadcrum.css'
 import arrow_icon from '../Assets/breadcrum_arrow.png'
 
 
+
 const Breadcrum = (props) => {
     const {product} = props;
   return (
     <div className='breadcrum'>
-        HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
+        HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" />  <img src={arrow_icon} alt="" /> 
     </div>
   )
 }
